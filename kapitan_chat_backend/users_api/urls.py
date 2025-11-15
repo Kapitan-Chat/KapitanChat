6f10a2d755119dc18045043b14bc0d0c064494f6
Azimkin <hayavim2009@gmail.com>
2025-11-15 16:53:03 +0100

    Add search request

--- kapitan_chat_backend/users_api/urls.py
+++ kapitan_chat_backend/users_api/urls.py
@@ -2,12 +2,13 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from users_api.views import RegisterView, GetMe, Users
+from .views import RegisterView, GetMe, Users, SearchApiView
 
 router = DefaultRouter()
 
 
 urlpatterns = [
+    path('search/', SearchApiView.as_view(), name='search_user'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),

dfda110f63067f017bee7e7dc29f241aaca01b5d
Azimkin <hayavim2009@gmail.com>
2025-12-04 20:41:53 +0100

    Implement nickname, email, phone validators

--- kapitan_chat_backend/users_api/views.py
+++ kapitan_chat_backend/users_api/views.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
-from drf_spectacular.utils import extend_schema, OpenApiParameter
-from rest_framework import status
+from drf_spectacular.utils import extend_schema, OpenApiParameter, inline_serializer
+from rest_framework import status, serializers
+from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView, UpdateAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import logging
+
+from rest_framework.viewsets import ViewSet
 
 from chat_main_api.models import Chat
 from .models import Profile
@@ -17,12 +20,13 @@ from .serializers import RegisterSerializer, UserSerializer, ProfileSerializer
 
 logger = logging.getLogger(__name__)
 
+
 # Create your views here.
 class RegisterView(CreateAPIView):
-    permission_classes = ()
+    permission_classes = [AllowAny]
+    authentication_classes = []
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -35,7 +39,6 @@ class RegisterView(CreateAPIView):
 class GetMe(APIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
-
 
     def get(self, request: Request) -> Response:
         user = request.user
@@ -52,7 +55,6 @@ class Users(RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
     queryset = User.objects.all()
-
 
     def get(self, request, *args, **kwargs):
         _id = self.kwargs['pk']
@@ -91,3 +93,92 @@ class ProfileView(UpdateAPIView):
     permission_classes = (IsAuthenticated, IsSelfRequest)
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ValidationViewSet(ViewSet):
+    __SuccessResponse__ = inline_serializer(
+        name='Success',
+        fields={
+            "available": serializers.BooleanField(),
+        }
+    )
+    __ErrorResponse__ = inline_serializer(
+        name='ValidationError',
+        fields={
+            "error": serializers.CharField(),
+        }
+    )
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="nickname",
+                type=str,
+                location='query',
+                required=True,
+            ),
+        ],
+        responses={
+            200: __SuccessResponse__,
+            422: __ErrorResponse__,
+        }
+    )
+    @action(detail=False, methods=['get'])
+    def nickname(self, request: Request):
+        nickname = request.query_params.get('nickname', None)
+        if nickname:
+            exists = User.objects.filter(username__iexact=nickname).exists()
+            return Response({"available": not exists}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "nickname query parameter is required!"},
+                            status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="phone",
+                type=str,
+                location='query',
+                required=True,
+            ),
+        ],
+        responses={
+            200: __SuccessResponse__,
+            422: __ErrorResponse__,
+        }
+    )
+    @action(detail=False, methods=['get'])
+    def phone(self, request: Request):
+        phone = request.query_params.get('phone', None)
+        if phone:
+            exists = Profile.objects.filter(phone_number__iexact=phone).exists()
+            return Response({"available": not exists}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "phone query parameter is required!"},
+                            status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="email",
+                type=str,
+                location='query',
+                required=True,
+            ),
+        ],
+        responses={
+            200: __SuccessResponse__,
+            422: __ErrorResponse__,
+        }
+    )
+    @action(detail=False, methods=['get'])
+    def email(self, request: Request):
+        email = request.query_params.get('email', None)
+        if email:
+            exists = User.objects.filter(email__iexact=email).exists()
+            return Response({"available": not exists}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "email query parameter is required!"},
+                            status=status.HTTP_422_UNPROCESSABLE_ENTITY)

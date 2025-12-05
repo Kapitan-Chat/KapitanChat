92e1038026aafde1992187e28a4e48c13a28c5ad
Azimkin <hayavim2009@gmail.com>
2025-12-05 01:52:33 +0100

    Split registration and login logic, add validation

--- kapitan_chat_frontend/src/login-register/authentication.jsx
+++ kapitan_chat_frontend/src/login-register/authentication.jsx
@@ -1,381 +1,222 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
-import LOGO from '../assets/logo.png';
-import styles from './authentication.module.css';
-import Login from './login';
-import Register from './register';
+import LOGO from "../assets/logo.png";
+import styles from "./authentication.module.css";
+import Login from "./login";
+import Register from "./register";
 
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../Provider/AuthProvider';
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../Provider/AuthProvider";
 
-const Authentication = () => {
-    const [mousePos, setMousePos] = useState({x: 0, y: 0});
+const Authentication = ({ authType = "login" }) => {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
 
-    const {
-        token,
-        setToken,
+  const onSubmit = useRef((e) => {});
 
-        login,
-        local,
-        settingparams,
-        setSettingparams,
-    } = useAuth();
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const authenticationRef = React.createRef();
 
-    // Поля для форми Логіну
-    const [loginUsername, setLoginUsername] = useState('');
-    const [loginPassword, setLoginPassword] = useState('');
+  const leftPupilRef = React.createRef();
+  const rightPupilRef = React.createRef();
 
-    const loginFormDetails = {
-        loginUsername,
-        setLoginUsername,
-        loginPassword,
-        setLoginPassword
+  const rightGlareRef = React.createRef();
+  const leftGlareRef = React.createRef();
+
+  const rightEyeLidRef = React.useRef();
+  const leftEyeLidRef = React.useRef();
+
+  const logoRef = React.useRef();
+
+  const chatBubbleRef = React.useRef();
+
+  let bubble_chat_animation = false;
+
+  const handleMouseMove = (posX, posY) => {
+    const rect = authenticationRef.current.getBoundingClientRect();
+    const x = posX - rect.left;
+    const y = posY - rect.top;
+    setMousePos({ x, y });
+
+    // Рух очей штурвалу
+    const pupils = [leftPupilRef.current, rightPupilRef.current];
+    const glares = [leftGlareRef.current, rightGlareRef.current];
+
+    if (pupils) {
+      const screenX = (x / rect.width) * 100;
+      const screenY = (y / rect.height) * 100;
+
+      pupils.forEach((pupil) => {
+        pupil.style.left = `${2.5 + (15 / 100) * screenX}px`;
+        pupil.style.top = `${2.5 + (15 / 100) * screenY}px`;
+      });
+
+      glares.forEach((glare) => {
+        glare.style.left = `${1.5 + (10 / 100) * screenX}px`;
+        glare.style.top = `${1.5 + (10 / 100) * screenY}px`;
+      });
+    }
+  };
+
+  function angry_animation() {
+    const newRightEye = [styles.eyelid, styles.angry_right_eye].join(" ");
+    const newLeftEye = [styles.eyelid, styles.angry_left_eye].join(" ");
+
+    let animationClear = () => {
+      if (
+        !leftEyeLidRef.current ||
+        !rightEyeLidRef.current ||
+        !logoRef.current
+      ) {
+        console.log("Some or all refs are null!");
+      } else {
+        console.log(rightEyeLidRef, leftEyeLidRef, logoRef);
+        rightEyeLidRef.current.className = styles.eyelid;
+        leftEyeLidRef.current.className = styles.eyelid;
+        logoRef.current.className = styles.logo;
+
+        console.log("Angry animation end!", rightEyeLidRef, leftEyeLidRef);
+      }
     };
 
+    if (
+      rightEyeLidRef.current.className != newRightEye &&
+      leftEyeLidRef.current.className != newLeftEye
+    ) {
+      rightEyeLidRef.current.className = newRightEye;
+      leftEyeLidRef.current.className = newLeftEye;
 
-    
+      logoRef.current.className = [styles.logo, styles.angry_logo].join(" ");
 
-    // Поля для форми Реєстрації
-    const [registerUsername, setRegisterUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [registerPassword, setRegisterPassword] = useState('');
-    const [registerConfirmPassword, setRegisterConfirmPassword] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
+      console.log("Angry animation activation!");
 
-    const registerFormDetails = {
-        registerUsername,
-        setRegisterUsername,
-        email,
-        setEmail,
-        registerPassword,
-        setRegisterPassword,
-        registerConfirmPassword,
-        setRegisterConfirmPassword,
-        firstName,
-        setFirstName,
-        lastName,
-        setLastName,
-        phoneNumber,
-        setPhoneNumber
+      setTimeout(animationClear, 3000);
+    } else console.log("Animation already in use!");
+  }
+
+  function happy_animation() {
+    const newRightEye = [styles.eyelid, styles.happy_right_eye].join(" ");
+    const newLeftEye = [styles.eyelid, styles.happy_left_eye].join(" ");
+
+    let animationClear = () => {
+      if (!leftEyeLidRef.current || !rightEyeLidRef.current) {
+        console.log("Some or all refs are null!");
+      } else {
+        rightEyeLidRef.current.className = styles.eyelid;
+        leftEyeLidRef.current.className = styles.eyelid;
+
+        console.log("Happy animation end!", rightEyeLidRef, leftEyeLidRef);
+      }
     };
 
+    if (
+      rightEyeLidRef.current.className != newRightEye &&
+      leftEyeLidRef.current.className != newLeftEye
+    ) {
+      rightEyeLidRef.current.className = newRightEye;
+      leftEyeLidRef.current.className = newLeftEye;
 
-    const [authType, setAuthType] = useState('login');
+      console.log("Happy animation activation!");
 
-    const authenticationRef = React.createRef();
+      setTimeout(animationClear, 3000);
+    } else console.log("Animation already in use!");
+  }
 
-    const leftPupilRef = React.createRef();
-    const rightPupilRef = React.createRef();
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit.current(e);
+  }
 
-    const rightGlareRef = React.createRef();
-    const leftGlareRef = React.createRef();
+  function LoginOrRegister(authType) {
+    const animations = {
+      animateText: bubble_text_animate,
+      angry: angry_animation,
+      happy: happy_animation,
+    };
+    const Component =
+      authType === "login" ? Login : authType === "register" ? Register : null;
 
-    const rightEyeLidRef = React.useRef();
-    const leftEyeLidRef = React.useRef();
+    return Component ? (
+      <Component onSubmit={onSubmit} animations={animations} />
+    ) : (
+      <p>authType null or wrong format</p>
+    );
+  }
 
-    const logoRef = React.useRef();
+  function bubble_text_animate(text) {
+    if (chatBubbleRef.current && text && !bubble_chat_animation) {
+      bubble_chat_animation = true;
+      let currentText = "Kapitan$\n\n";
+      let index = 0;
 
-    const chatBubbleRef = React.useRef();
+      let animation = setInterval(() => {
+        currentText = currentText.slice(0, -1);
+        currentText += text[index++] + "_";
 
-    let bubble_chat_animation = false;
+        //console.log("Writing text:", currentText);
 
-    const handleMouseMove = (posX, posY) => {
-        const rect = authenticationRef.current.getBoundingClientRect();
-        const x = posX - rect.left;
-        const y = posY - rect.top;
-        setMousePos({x, y});
+        chatBubbleRef.current.innerText = currentText;
 
-        // Рух очей штурвалу
-        const pupils = [leftPupilRef.current, rightPupilRef.current];
-        const glares = [leftGlareRef.current, rightGlareRef.current];
-
-        if (pupils) {
-            const screenX = x / rect.width * 100;
-            const screenY = y / rect.height * 100;
-
-            pupils.forEach(pupil => {
-                pupil.style.left = `${2.5 + (15 / 100 * screenX)}px`;
-                pupil.style.top = `${2.5 + (15 / 100 * screenY)}px`;
-            });
-
-            glares.forEach(glare => {
-                glare.style.left = `${1.5 + (10 / 100 * screenX)}px`;
-                glare.style.top = `${1.5 + (10 / 100 * screenY)}px`;
-            });
-            
+        if (index == text.length) {
+          clearInterval(animation);
+          currentText = currentText.slice(0, -1);
+          chatBubbleRef.current.innerText = currentText;
+          bubble_chat_animation = false;
         }
-    }
+      }, 40);
+    } else
+      console.log(
+        "ChatBubble or text is null, or another animation is in progress"
+      );
+  }
 
-    function angry_animation(){
-        const newRightEye = [styles.eyelid, styles.angry_right_eye].join(' ');
-        const newLeftEye = [styles.eyelid, styles.angry_left_eye].join(' ');
+  useEffect(() => {
+    const st = setTimeout(() => {
+      bubble_text_animate("Welcome to Kapitan Chat! Login or register");
+    }, 500);
 
-        let animationClear = () => {
-            if(!leftEyeLidRef.current || !rightEyeLidRef.current || !logoRef.current){
-                console.log("Some or all refs are null!");  
-            }else{
-                console.log(rightEyeLidRef, leftEyeLidRef, logoRef)
-                rightEyeLidRef.current.className = styles.eyelid;
-                leftEyeLidRef.current.className = styles.eyelid;
-                logoRef.current.className = styles.logo;
+    return () => {
+      clearTimeout(st);
+    };
+  }, []);
 
-                console.log("Angry animation end!", rightEyeLidRef, leftEyeLidRef);
-            }
-                 
-        };
+  return (
+    // Головний контейнер для логіна та реєстрації
+    <div
+      className={styles.authentication_container}
+      onMouseMove={(e) => handleMouseMove(e.clientX, e.clientY)}
+      ref={authenticationRef}
+    >
+      {/* Універсальна форма, шлях для запиту передається через пропси */}
+      <form className={styles.authentication_form} onSubmit={handleSubmit}>
+        {/* Хмарина з повідомленнями штурвалу */}
+        <div className={styles.chat_bubble} ref={chatBubbleRef}></div>
 
-        if(
-            rightEyeLidRef.current.className != newRightEye
-            &&
-            leftEyeLidRef.current.className != newLeftEye
-        ){
-            rightEyeLidRef.current.className = newRightEye;
-            leftEyeLidRef.current.className = newLeftEye;
+        {/* Накурений штурвал */}
+        <img src={LOGO} alt="logo" className={styles.logo} ref={logoRef} />
 
-            logoRef.current.className = [styles.logo, styles.angry_logo].join(' ');
-
-            console.log("Angry animation activation!");
-
-            setTimeout(animationClear, 3000);
-            
-        }else
-            console.log("Animation already in use!")
-        
-    }
-
-    function happy_animation(){
-        const newRightEye = [styles.eyelid, styles.happy_right_eye].join(' ');
-        const newLeftEye = [styles.eyelid, styles.happy_left_eye].join(' ');
-
-        let animationClear = () => {
-            if(!leftEyeLidRef.current || !rightEyeLidRef.current){
-                console.log("Some or all refs are null!");  
-            }else{
-                rightEyeLidRef.current.className = styles.eyelid;
-                leftEyeLidRef.current.className = styles.eyelid;
-
-                console.log("Happy animation end!", rightEyeLidRef, leftEyeLidRef);
-            }
-                 
-        };
-
-        if(
-            rightEyeLidRef.current.className != newRightEye
-            &&
-            leftEyeLidRef.current.className != newLeftEye
-        ){
-            rightEyeLidRef.current.className = newRightEye;
-            leftEyeLidRef.current.className = newLeftEye;
-
-            console.log("Happy animation activation!");
-
-            setTimeout(animationClear, 3000);
-            
-        }else
-            console.log("Animation already in use!")
-        
-    }
-
-    // Запит на створення нового юзера. При успішному створенні одразу переходить до входу
-    function RegisterRequest(details){
-
-        axios.post(`${import.meta.env.VITE_BASEAPI}users/register/`, details)
-        .then(response => {
-
-        console.log("Реєстрація успішна!");
-
-        LoginRequest({
-            "username" : details["username"],
-            "password" : details["password"]
-        });
-
-        })
-        .catch(error => {
-
-        console.error('There was an error!', error);
-        
-        if([registerUsername, firstName, email, registerPassword].includes('')){
-            bubble_text_animate("Fill in all the fields!");
-            angry_animation();
-            return;
-        }else if(registerConfirmPassword != registerPassword){
-            bubble_text_animate("Passwords do not match!");
-            angry_animation();
-            return;
-        }
-
-        });
-    }
-
-    // Запит на перевірку вхідних данних та отримання JWT
-    function LoginRequest(details){
-        axios.post(`${import.meta.env.VITE_BASEAPI}users/token/`, details)
-        .then(response => {
-            console.log("Вхід успішний!", response.data);
-            
-            const access = response.data.access;
-            const refresh = response.data.refresh;
-
-            localStorage.setItem("access", access);
-            localStorage.setItem("refresh", refresh);
-            
-            setToken({
-                JWTaccessToken: access,JWTrefreshToken: refresh
-            });
-
-            login();
-            
-            bubble_text_animate("Welcome!");
-            happy_animation();
-
-            navigate('/main');
-        })
-        .catch(error => {
-            console.error('There was an error!', error);
-            bubble_text_animate("Wrong username or password! Are you a hacker??");
-            angry_animation();
-        });
-    }
-
-    function handleSubmit(e){
-        e.preventDefault();
-
-        const registerDetails = {
-            "username": registerUsername,
-            "first_name": firstName,
-            "last_name": lastName,
-            "email": email,
-            "password": registerPassword,
-            "phone_number": phoneNumber
-        };
-
-        const loginDetails = {
-            "username": loginUsername,
-            "password": loginPassword
-        };
-
-        if(authType === 'register'){
-            RegisterRequest(registerDetails);
-        }
-
-        if(authType == 'login'){
-            LoginRequest(loginDetails);
-            
-        }
-    }
-
-    function LoginOrRegister(authType){
-        if(authType){
-            if(authType === 'login')
-               return <Login 
-                    auth = {[authType, setAuthType]}
-                    formDetails={loginFormDetails}
-                />
-            
-            if(authType === 'register')
-                return <Register 
-                    auth = {[authType, setAuthType]}
-                    formDetails={registerFormDetails}
-                />
-        }else
-            return <p>authType null or wrong format</p>
-    }
-
-    function bubble_text_animate(text){
-   
-
-        if(chatBubbleRef.current && text && !bubble_chat_animation){
-            bubble_chat_animation = true;
-            let currentText = "Kapitan$\n\n";
-            let index = 0;
-
-            let animation = setInterval(() => {
-                currentText = currentText.slice(0, -1);
-                currentText += text[index++] + '_';
-                
-                console.log("Writing text:", currentText);
-                
-                chatBubbleRef.current.innerText = currentText;
-            
-                if(index == text.length){
-                    clearInterval(animation);
-                    currentText = currentText.slice(0, -1);
-                    chatBubbleRef.current.innerText = currentText;
-                    bubble_chat_animation = false;
-                }
-            }, 40)
-
-        }else
-            console.log("ChatBubble or text is null, or another animation is in progress")
-    }
-
-    useEffect(() => {
-        const st = setTimeout(() => {
-            bubble_text_animate("Welcome to Kapitan Chat! Login or register")
-        }, 500)
-        
-        return ()=>{clearTimeout(st)}
-    }, []);
-
-
-    
-    return (
-        // Головний контейнер для логіна та реєстрації 
-        <div 
-            className={styles.authentication_container}
-            onMouseMove={(e) => handleMouseMove(e.clientX, e.clientY)}
-            ref={authenticationRef}
-        >
-            
-            {/* Універсальна форма, шлях для запиту передається через пропси */}
-            <form 
-                className={styles.authentication_form}
-                onSubmit={handleSubmit}
-            >
-
-                {/* Хмарина з повідомленнями штурвалу */}
-                <div className={styles.chat_bubble} ref={chatBubbleRef}></div>
-
-
-                {/* Накурений штурвал */}
-                <img src={LOGO} alt="logo" className={styles.logo} ref={logoRef}/>
-
-                {/* Очі штурвалу, які рухаються */}
-                <div className={styles.eyes_container}>
-                    <div className={[styles.eye, styles.left_eye].join(' ')}>
-                        <div className={styles.eyelid} ref={leftEyeLidRef}></div>
-                        <div 
-                            className={styles.pupil} 
-                            ref={leftPupilRef}
-                        >
-                            <div className={styles.glare} ref={leftGlareRef}></div>
-                        </div>
-
-                    </div>
-                    <div className={[styles.eye, styles.right_eye].join(' ')}>
-                        <div className={styles.eyelid} ref={rightEyeLidRef}></div>
-                        <div 
-                            className={styles.pupil} 
-                            ref={rightPupilRef}
-                        >
-                            <div className={styles.glare} ref={rightGlareRef}></div>
-                        </div>
-                    </div>
-                </div>
-
-                {/* Контент форми (Логін або Реєстрація) */}
-                {LoginOrRegister(authType)}
-
-            </form>
+        {/* Очі штурвалу, які рухаються */}
+        <div className={styles.eyes_container}>
+          <div className={[styles.eye, styles.left_eye].join(" ")}>
+            <div className={styles.eyelid} ref={leftEyeLidRef}></div>
+            <div className={styles.pupil} ref={leftPupilRef}>
+              <div className={styles.glare} ref={leftGlareRef}></div>
+            </div>
+          </div>
+          <div className={[styles.eye, styles.right_eye].join(" ")}>
+            <div className={styles.eyelid} ref={rightEyeLidRef}></div>
+            <div className={styles.pupil} ref={rightPupilRef}>
+              <div className={styles.glare} ref={rightGlareRef}></div>
+            </div>
+          </div>
         </div>
-     );
-}
- 
 
+        {/* Контент форми (Логін або Реєстрація) */}
+        {LoginOrRegister(authType)}
+      </form>
+    </div>
+  );
+};
 
 export default Authentication;
--- kapitan_chat_frontend/src/login-register/login.jsx
+++ kapitan_chat_frontend/src/login-register/login.jsx
@@ -1,35 +1,65 @@
-import styles from './authentication.module.css';
+import { useNavigate } from "react-router-dom";
+import styles from "./authentication.module.css";
+import { useState } from "react";
+import { useAuth } from "../Provider/AuthProvider";
+import { loginRequest } from "./requests";
 
-const Login = ({auth, formDetails}) => {
-    const [authType, setAuthType] = auth;
-    const {
-        loginUsername,
-        setLoginUsername,
-        loginPassword,
-        setLoginPassword
-    } = formDetails;
+const Login = ({ onSubmit, animations }) => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [isInProcess, setInProcess] = useState(false);
+  const auth = useAuth();
 
-    return ( 
-        <div className={styles.login_content}>
-            <h2 className={styles.login_title}>Login</h2>
-            <div className={styles.login_fields}>
-                <input 
-                    type="text" 
-                    placeholder='Enter your username'
-                    value={loginUsername}
-                    onChange={(e) => setLoginUsername(e.target.value)}
-                />
-                <input 
-                    type="password" 
-                    placeholder='Enter your password'
-                    value={loginPassword}
-                    onChange={(e) => setLoginPassword(e.target.value)}
-                />
-            </div>
-            <button className={styles.login_button}>Login</button>
-            <p className={styles.bottom_text}>Don't have account? <span onClick={() => setAuthType('register')}>Create account</span></p>
-        </div> 
-    );
-}
- 
+  const navigate = useNavigate();
+
+  onSubmit.current = async (e) => {
+    setInProcess(true);
+    loginRequest({ username: login, password }, auth, (isSuccess) => {
+      if (isSuccess) {
+        navigate("/main");
+        setInProcess(false);
+      } else {
+        animations.animateText(
+          "Wrong username or password! Are you a hacker??"
+        );
+        animations.angry();
+        setInProcess(false);
+      }
+    });
+  };
+
+  return (
+    <div className={styles.login_content}>
+      <h2 className={styles.login_title}>Login</h2>
+      <div className={styles.login_fields}>
+        <input
+          type="text"
+          placeholder="Enter your username"
+          value={login}
+          required
+          onChange={(e) => setLogin(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button
+        type="submit"
+        disabled={isInProcess}
+        className={styles.login_button}
+      >
+        {isInProcess ? "Logging in..." : "Login"}
+      </button>
+      <p className={styles.bottom_text}>
+        Don't have account?{" "}
+        <span onClick={() => navigate("/register")}>Create account</span>
+      </p>
+    </div>
+  );
+};
+
 export default Login;
